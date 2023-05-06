5dc2a0859437d33c7efa527023f28fadd6394269
Kelvin M Matheka <beingmatheka@gmail.com>
2023-05-06 16:46:29 +0300

    Done with Category Endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express, {Request, Response} from "express";
 
 const app = express();
+
+//port
 const PORT = 3000
 
-app.get(',', (req:Request, res:Response) => {
-  res.send('Hello World');
+//json middleware
+app.use(express.json());
+
+//route for categories
+app.get('/api/categories', (req:Request, res:Response) => {
+  const categories = [
+    { id: 1, name: 'Women' },
+    { id: 2, name: 'Men' },
+    { id: 3, name: 'Kid' },
+  ];
+  res.json(categories);
 })
 
 app.listen(PORT, () => {
