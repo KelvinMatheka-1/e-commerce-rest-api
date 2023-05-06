4e8f24fd534104a56885b903a07335479ad2e172
Kelvin M Matheka <beingmatheka@gmail.com>
2023-05-06 21:43:24 +0300

    populating items

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,20 @@ app.get('/api/categories', (req:Request, res:Response) => {
   res.json(categories);
 })
 
+//route for all items
+app.get('/api/items', (req:Request, res:Response)=>{
+  const items = [
+    {id: 1, item_name: 'Apollo Running Short', img_url: './images/image.png', price: 50, category: "Women"},
+    {id: 2, item_name: 'Apollo Running Short', img_url: './images/image3.png', price: 50,category: "Men"},
+    {id: 3, item_name: 'Apollo Running Short', img_url: './images/image1.png', price: 50, category: "Women"},
+    {id: 4, item_name: 'Apollo Running Short', img_url: './images/image3.png', price: 50, category: "Kids"},
+    {id: 5, item_name: 'Apollo Running Short', img_url: './images/image1.png', price: 50, category: "Men"},
+    {id: 6, item_name: 'Apollo Running Short', img_url: './images/image2.png', price: 50, category: "Kids"},
+    
+  ];
+  res.json(items);
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
